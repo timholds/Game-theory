a4fc212a83c63d6d30c3c3adaf4ba8859aaa1582
timholds <tholdsworth@sandiego.edu>
2018-01-08 01:16:21 -0800

    Got a derivative working

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
 # Code to solve a single game between two players
 # Tim Holdsworth, January 7st, 2018
 from sympy import *
+Q = Symbol('Q')
+p = Symbol('p')
+k = Symbol('k')
+q = Symbol('q')
+a = Symbol('a')
+s = Symbol('s')
+w = Symbol('w')
+x = Symbol('x')
+
 
 # What are the variables a class should share
 # What are the variables an instance of a class should share
@@ -49,47 +58,57 @@ class Player():
         print("getter of x called")
         return self._utility
 
-    @x.setter
+    @utility.setter
     def utility(self, value):
         print("setter of x called")
         self._utility = value
 
-    @x.deleter
+    @utility.deleter
     def utility(self):
         print("deleter of x called")
         del self._utility
 
     # Example of how to use
-    p1 = Player()
-    p1.utility = 'foo'  # setter called
-    foo = p1.utility  # getter called
-    del p1.utility  # deleter called
+    #p1 = Player()
+    #p1.utility = 'foo'  # setter called
+    #foo = p1.utility  # getter called
+    #del p1.utility  # deleter called
 
 
     # A function for optimizing utility
     def optimize_utility(self):
         pass
+
     # A function that defines utility function
     def utility_function(self):
         pass
 
-    @setattr()
-    def profit(self):
-        pass
+def generate_utilty_function(player, *cases):
+    q = Symbol('q')
+    a = Symbol('a')
+    s = Symbol('s')
+    w = Symbol('w')
+    wg = Symbol('wg')
+    for case in cases:
+        if player == 'Consumer':
+            pass
+        elif player == 'Retailer':
+            pass
+        elif player == 'Farmer':
+            pass
+
+
 
 # Function to calculate consumer_demand Q in a given case for Stage4Games
 def consumer_demand_function(*cases):
     ''' Function to determine Q in a given case for a given set of params q, a, s, and k
         Example function call would be consumer_demand_function(cases[0]) for FTOrg case'''
 
-    # TODO initialize these to the values that you want them to be
-    q, a, s, k = None
-    P = None
 
     for case in cases:
         # The fair trade cases
         if case == 'FTOrg':
-            pass
+            q = q
         elif case == 'FTConv':
             q = 0 # since
             a = 1
@@ -103,35 +122,49 @@ def consumer_demand_function(*cases):
             print('Case not recognized')
 
         # Q is consumer demand function that is true in all cases. Values of a, s,
-        Q = k + q + a * s - P
+        Q = k + q - p + (a * s)
 
-        print('The consumer demand in case {case} '.format(case) + 'is' + str(Q))
+        print('The consumer demand in case {} '.format(case) + 'is ' + str(Q))
         return Q
 
 # Function to calculate retailer_profit pi_r in a given case for Stage4Games
 def retailer_profit_function(*cases):
+    w = Symbol('w')
+    wg = Symbol('wg')
+    wc = Symbol('wg')
+    w = wg + wc
+    wholesale_price = None
     for case in cases:
         # The fair trade cases
         if case == 'FTOrg':
             consumer_demand = consumer_demand_function(case)
+            wholesale_price = w
         elif case == 'FTConv':
             consumer_demand = consumer_demand_function(case)
+            wholesale_price = w
         # The non fair trade cases
         elif case == 'NFTOrg':
             consumer_demand = consumer_demand_function(case)
+            wholesale_price = wg
         elif case == 'NFTConv':
             consumer_demand = consumer_demand_function(case)
+            wholesale_price = wc
         else:
             print('Case not recognized')
 
-        retail_price = P
-        wholesale_price = w+wg
+        # Retail price is known since we know p* from stage4game, yes?
+        retail_price = p
 
         retailer_profit = (retail_price - wholesale_price) * consumer_demand
         return retailer_profit
 
+
+    #consumer = Player()
+    #consumer.utility = consumer_utility_function(cases[0[)
     #retailer = Player()
     #retailer.utility = retailer_profit_function(cases[0])
+    # farmer = Player()
+    # farmer.utilty = farmer_profit_function(cases[0])
 
 # Function to calculate farmer_profit pi_f in a given case for Stage2&3Games
 def farmer_profit_function(*cases):
@@ -153,25 +186,30 @@ def farmer_profit_function(*cases):
         else:
             print('Case not recognized')
 
+        # TODO figure out how to store these Symbol things so all functions have access to them
+        Qhat = Symbol('Qhat')
+        b = Symbol('b')
+        wc = Symbol('wc')
+        w = Symbol('w')
+        wg = Symbol('wg')
+
         wholesale_price = wc + w + wg
         production_cost = a + b*Q
+        quantity_sold = Q
+        quantity_produced = Qhat
 
-        farmer_profit_function = wholesale_price - production_cost
-        return farmer_profit_function
+
+        farmer_profit = wholesale_price*quantity_sold - production_cost*quantity_produced
+        print('In case {}, farmer profit function is '.format(case) + str(farmer_profit))
+        return farmer_profit
+
+farmer_profit_function(cases[0])
 
     # farmer = Player
     # farmer_profit_function = farmer_profit_function(case[0])
     # farmer.utilty = farmer_profit_function
     # farmer.maximize_utility() -returns utility function as an expression optimized for one value
 
-def main():
-
-    consumer = Player()
-    retailer = Player()
-    farmer = Player()
-
-    4thstageGameCase1 = Game()
-    4thstageGameCase2 = Game()
 
 # utility functions are already determined for all players*game*stage
 # --- For any game at any stage, we know the utility for any player
@@ -185,20 +223,6 @@ def main():
 
 # What is the outcome of a stage-game? Decision maker has a decision boundary piecewise
 
-for case in cases:
-
-    consumer = Player()
-    retailer = Player()
-    farmer = Player()
-
-    game4 = 4thStageGame(conumser, retailer)
-    sol4 = game4.solver()
-    game3 = 3rdstagegame(retailer, farmer)
-    sol3 = game3.solver()
-    game2 = 2thstagegame(retailer, farmer)
-    sol2 = game2.solver()
-    game1 = 1ststagegame(farmer, retailer)
-    sol1 = game1.solver()
 
 class Game():
     ''' A Game Object for a single two player game'''
@@ -217,21 +241,15 @@ class FirstStageGame(Game):
 
     def __init__(self, player1, player2, p1utility_function, p2utility_function):
         Game.__init__(self, player1, player2)
-        self.p1utility = consumer_demand_function(cases[case])
+        self.p1utility = consumer_demand_function(cases[0])
         self.p2utility = p2utility_function
 
 
 
-    #retailer = Player()
-    retailer.utility = retailer_profit_function(cases[case])
+    retailer = Player()
+    retailer.utility = retailer_profit_function(cases[0])
     consumer = Player()
-    consumer.utility = consumer_demand_function(cases[case])
-
-
-# Consistent over all
-consumer.profit = utility - retail_price
-retailer.profit = retail_price - wholesale_price
-farmer.profit =  wholesale_price - production_cost
+    consumer.utility = consumer_demand_function(cases[0])
 
 
 # all games have players, all players have utility functions
@@ -247,8 +265,47 @@ class FourthStageGame(Game):
     def GetEmployee(self):
         return self.Name() + ", " + self.staffnumber
 
+# a soln should inherit w from its game
+# a soln maps multiple eq with mult variables into less eq with less variables
+def substitute2(objective_equation, sub_equation, oldvar, newvar):
+    f1 = objective_equation
+    f2 = sub_equation
+    f3 = f1.subs(oldvar, newvar)
+    print('swag')
+    print(f3)
 
 
+#(Q = k + q - p + (a * s)), (p * Q - w * Q), p, Q)
+#substitute2()
+
+
+def substitute(case, oldvar, newvar):
+    ''' A substitution'''
+
+    #Q = consumer_demand_function(case) # Q = k + q - p + (a * s)
+    #print(str(Q))
+    #Q = k + q - p + (a * s)
+
+    # TODO make retailer_profit_function(case) that returns p*Q - w*Q
+    # r_profit = retailer_profit_function(case) #p*Q - w*Q
+    r_profit = p * Q - w * Q
+    print('Retailer profit is1aja: ' + str(r_profit))
+
+    # Substitute old variables in for new one in the profit equation
+    r_profit_sub = r_profit.subs(oldvar, newvar)
+    print('Retailer profit in terms of {} (where {} substituted out)'.format(newvar, oldvar) + 'is: ' + str(
+        r_profit_sub))
+
+    # Calculate the derivative of the post-substitution function wrt newvar
+    deriv = diff(r_profit_sub, newvar)
+    print('In terms of {}, derivative of Retailer profit: '.format(newvar) + str(deriv))
+
+    max = solve(deriv, newvar)
+    print('Max retail profit occurs when {} = '.format(newvar) + 'is: ' + str(max[0]))
+    print('yo')
+    return max
+
+substitute(cases[0], Q, p)
 
 
 
@@ -280,7 +337,7 @@ def constrained_optimzation(function, variable, **kwargs):
     unconstrained_max = unconstrained_optimization(function, variable)
     for arg in kwargs:
         pass
-    if unconstrained_max.fits_constraints() == True
+    #if unconstrained_max.fits_constraints() == True
 
 # Let the arguments be the constraints, if they exist
 def find_max(function, variable, **kwargs):
@@ -306,3 +363,22 @@ def __init__(self, q, a, s, k):
     self.a = a
     self.s = s
     self.k = k
+
+
+
+"""
+for case in cases:
+
+    consumer = Player()
+    retailer = Player()
+    farmer = Player()
+
+    game4 = 4thStageGame(consumser, retailer)
+    sol4 = game4.solver()
+    game3 = 3rdstagegame(retailer, farmer)
+    sol3 = game3.solver()
+    game2 = 2thstagegame(retailer, farmer)
+    sol2 = game2.solver()
+    game1 = 1ststagegame(farmer, retailer)
+    sol1 = game1.solver()
+"""
--- sympy_test.py
+++ sympy_test.py
@@ -9,7 +9,7 @@ from optlang import Model, Variable, Constraint, Objective
 
 x = Symbol('x')
 sol = solve(x**2 - 1, x)
-print(sol)
+#print(sol)
 
 Q = Symbol('Q')
 p = Symbol('p')
@@ -19,25 +19,37 @@ a = Symbol('a')
 s = Symbol('s')
 w = Symbol('w')
 
+
 soln2 = solve(k + q - p + a * s, p)
 soln = solve(Q**3 - 1, Q)
-print(soln2)
+#print(soln2)
 
-Q = k + q - p + (a*s)
 
-def soln(wrtvariable=None):
-    # Need to have all the exog variables here
-    # should be help by some data thing somewhere and give by user
-    pi_r = p*Q - w*Q
-    pi_r_wrt_Q = pi_r.subs(p, Q)
-    var = wrtvariable
-    print('Retailer profit in terms of' + '{}'.format(var) + 'is: ' + str(pi_r_wrt_Q))
-    deriv = diff(pi_r_wrt_Q, p)
-    max = solve(deriv)
-    print(max)
+# a soln should inherit w from its game
+# a soln maps multiple eq with mult variables into less eq with less variables
+def soln(oldvar, newvar, case='FTOrg'):
+    ''' A solver for a single backwards induction game'''
+
+    # Q = consumer_demand_function(case) # Q = k + q - p + (a * s)
+    Q = k + q - p + (a * s)
+
+    # TODO make retailer_profit_function(case) that returns p*Q - w*Q
+    #r_profit = retailer_profit_function(case) #p*Q - w*Q
+    r_profit = p*Q - w*Q
+    print('Retailer profit is: ' + str(r_profit))
+
+    # Substitute old variables in for new one in the profit equation
+    r_profit_sub = r_profit.subs(oldvar, newvar)
+    print('Retailer profit in terms of {} (where {} substituted out)'.format(newvar, oldvar) + 'is: ' + str(r_profit_sub))
+
+    deriv = diff(r_profit_sub, newvar)
+    print('In terms of {}, derivative of Retailer profit: '.format(newvar) + str(deriv))
+
+    max = solve(deriv, newvar)
+    print('Max retail profit occurs when {} = '.format(newvar) + 'is: ' + str(max[0]))
     return max
 
-soln(p)
+soln(Q, p)
 # Get the computer to take the symbolic derivative
 
 # Solve for the derivative to find the max
